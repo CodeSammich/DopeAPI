09ea23e5947e3e72c719fc267af8991538a4a3f7
Samuel Zhang <samuelzhang3@gmail.com>
2015-11-10 13:16:06 -0500

    commenting, formatting

--- app.py
+++ app.py
@@ -8,26 +8,32 @@ app = Flask(__name__)
 @app.route("/<query>",methods=["GET","POST"])
 def main(query ="radiohead"):
     #Default query to radiohead
-    #
     basic = """http://developer.echonest.com/api/v4/artist/search
     ?api_key=V9SVA3AEDH6NCGYXY&format=json&name=""" + query + """&results=1"""
+    
     #basic API call for searching for artist names
     requesta = urllib2.urlopen(basic)
     resulta = requesta.read()
     query = json.loads(resulta)["response"]["artists"]["artist"][0]["name"]
+    
     #get first name from API return
     url="""http://developer.echonest.com/api/v4/artist/images?
     api_key=V9SVA3AEDH6NCGYXY&name=""" + query + """&format=json"""
+    
     #basic API call for search
     request = urllib2.urlopen(url)
     result = request.read()
-    r = json.loads(result)["response"]["images"]
+    r = json.loads(result)["response"]["images"] #gets images from image dictionary
+
+    #creates array of image urls to reference
     final = []
     for image in r:
         final.append(image["url"])
     if len(final) >= 15:
         final = final[0:15]
-    artist = query
+        
+    artist = query #artist names
+    
     return render_template("new.html",images=final,artist=artist)
 
 if (__name__ == "__main__"):
