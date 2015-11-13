1cc99b2c64b4cabef5032bd8fbf40a2962c6b462
Albert Mokrejs <albertmokrejs@gmail.com>
2015-11-12 19:14:40 -0500

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def apiCall(n):
 
 @app.route("/",methods=["GET","POST"])
 def main():
-    if request.method == "POST":
+    if request.method != "GET":
     	query = request.form['artist']
     else:
     	return render_template("Artist.html")
